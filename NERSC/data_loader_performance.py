dd049cc48ca2473aa48ce1117229cca416d30afa
avencastmini <yulei@cern.ch>
2025-03-31 21:51:22 +0200

    Refactor Ray initialization and enhance data processing output

--- NERSC/data_loader_performance.py
+++ NERSC/data_loader_performance.py
@@ -13,17 +13,17 @@ from ray.tune.schedulers import AsyncHyperBandScheduler
 from evenet.dataset.preprocess import process_event_batch
 
 if __name__ == '__main__':
-    ray.init(
-        # num_cpus=10,
-        # object_store_memory=10 * 1024 * 1024,
-    )
-
     # Step 1: Read the parquet files
     base_dir = Path("/global/cfs/cdirs/m2616/tihsu/PreTrain_Parquet")
 
     parquet_files = [
         str(base_dir / file) for file in base_dir.glob("*.parquet")
     ]
+
+    ray.init(
+        num_cpus=len(parquet_files),
+        # object_store_memory=10 * 1024 * 1024,
+    )
 
     ds = ray.data.read_parquet(parquet_files)
 
@@ -34,5 +34,8 @@ if __name__ == '__main__':
         # batch_format="default"
     )
 
-
     print(processed_ds.stats)
+
+    for i, batch in enumerate(processed_ds.iter_batches(batch_size=1024)):
+        # Each batch is a list of tuples as returned above
+        print("Batch ", i)
