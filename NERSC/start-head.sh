8741ea4bb59152811eb6ac4f3d17acd280193fc8
avencastmini <yulei@cern.ch>
2025-03-31 21:43:10 +0200

    Add customizable dashboard port for Ray head node startup

--- NERSC/start-head.sh
+++ NERSC/start-head.sh
@@ -3,7 +3,10 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-echo "starting ray head node"
+# Choose a dashboard port (or randomize it safely)
+PORT=8265
+
+echo "Ray will start on $(hostname) at port $PORT" > "$SCRATCH"/ray_dashboard_info.txt
 # Launch the head node
-ray start --head --node-ip-address="$1" --port=6379
+ray start --head --dashboard-host 0.0.0.0 --port=6379 --dashboard-port=$PORT
 sleep infinity
