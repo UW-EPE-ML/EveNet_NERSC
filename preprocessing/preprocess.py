7cfd715cfded4de4fa93064d897a78ec54028e12
avencastmini <yulei@cern.ch>
2025-04-03 16:43:46 +0200

    Refactor directory processing in preprocess.py; use f-string for improved readability

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -186,10 +186,10 @@ def main(cfg):
         for pretrain_dir in cfg.pretrain_dirs:
             for run_folder in Path(pretrain_dir).iterdir():
                 if run_folder.is_dir():
-                    in_tag = Path(pretrain_dir).name + "_" + run_folder
+                    in_tag = f"{Path(pretrain_dir).name}_{run_folder}"
                     print(f"[INFO] Processing {in_tag}")
 
-                preprocess(run_folder, cfg.store_dir, config.process_info, unique_id=in_tag)
+                    preprocess(run_folder, cfg.store_dir, config.process_info, unique_id=in_tag)
 
     else:
         in_tag = Path(cfg.in_dir).name
