1a7af73dde4e04c0b1c09c3ded1273636915ddfb
avencastmini <yulei@cern.ch>
2025-04-05 18:01:26 +0200

    Refactor logging of confusion matrix metrics for clarity

--- evenet/engine.py
+++ evenet/engine.py
@@ -216,7 +216,7 @@ class EveNetEngine(L.LightningModule):
                 fig = self.confusion_accumulator.plot(class_names=self.num_classes)
                 self.logger.experiment.log({"confusion_matrix": wandb.Image(fig)})
                 plt.close(fig)
-                self.log({
+                self.log(**{
                     "conf_matrix/valid": self.confusion_accumulator.valid,
                     "conf_matrix/total": self.confusion_accumulator.total,
                     "conf_matrix/fraction_used": self.confusion_accumulator.valid / self.confusion_accumulator.total,
