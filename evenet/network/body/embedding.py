Fix norm_mask usage in mean and std initialization to ensure correct normalization

evenet/network/body/embedding.py
@@ -292,7 +292,14 @@ class PETBody(nn.Module):
         ])
 
     def forward(self, input_features: Tensor, input_points: Tensor, mask: Tensor, time=Tensor) -> Tensor:
+        """
 
+        :param input_features: input features (batch_size, num_objects, num_features)
+        :param input_points: subset of input features that used to do edge calculation (batch_size, num_objects, num_local_features)
+        :param mask:  input features mask (batch_size, num_objects, 1)
+        :param time: time input for diffusion model usage.
+        :return:
+        """
         encoded = self.random_drop(input_features)
         encoded = self.feature_embedding(encoded)
 
@@ -305,7 +312,7 @@ class PETBody(nn.Module):
         encoded = torch.add(torch.mul(encoded, (1.0 + scale)), shift)
 
         if hasattr(self, 'local_embedding'):
-            points = input_points[:, :, :2]
+            points = input_points
             local_features = input_features
             local_features = self.local_embedding(local_features, points, mask)
             encoded = local_features + encoded  # Combine with original features

evenet/network/evenet_model.py
@@ -96,7 +96,7 @@ class EvenetModel(nn.Module):
         )
         # [1] Body
         self.PET_body = PETBody(
-            num_feat=len(self.local_feature_indices),
+            num_feat=self.sequential_input_dim,
             num_keep=self.options.Network.num_feature_keep,
             feature_drop=self.options.Network.PET_drop_probability,
             projection_dim=self.options.Network.hidden_dim,
@@ -257,7 +257,7 @@ class EvenetModel(nn.Module):
 
         global_conditions = self.global_embedding(global_conditions, global_conditions_mask)
         local_feature = input_point_cloud[..., self.local_feature_indices]
-        input_point_cloud = self.PET_body(local_feature, input_point_cloud, input_point_cloud_mask, time)
+        input_point_cloud = self.PET_body(input_point_cloud, local_feature, input_point_cloud_mask, time)
 
         ######################################
         ## Embedding for deterministic task ##