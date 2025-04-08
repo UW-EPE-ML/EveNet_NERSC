Refactor assignment handling: rename variables for clarity and fix detection reference in loss computation

evenet/network/evenet_model.py
@@ -168,7 +168,7 @@ class EveNetModel(nn.Module):
 
         if self.include_assignment:
         # [5] Assignment Head
-            self.multiprocess_assign_head = SharedAssignmentHead(
+            self.Assignment = SharedAssignmentHead(
                     resonance_particle_properties_mean=self.event_info.resonance_particle_properties_mean,
                     resonance_particle_properties_std=self.event_info.resonance_particle_properties_std,
                     pairing_topology=self.event_info.pairing_topology,
@@ -301,7 +301,7 @@ class EveNetModel(nn.Module):
         detections = dict()
         #
         if self.include_assignment:
-            assignments, detections, event_token = self.multiprocess_assign_head(
+            assignments, detections, event_token = self.Assignment(
                 x=embeddings,
                 x_mask=input_point_cloud_mask,
                 global_condition=embedded_global_conditions,

evenet/playground_new.py
@@ -152,6 +152,7 @@ shape_metadata = json.load(
     open("/Users/avencastmini/PycharmProjects/EveNet/workspace/test_data/test_output/shape_metadata.json"))
 
 normalization_dict = torch.load("/Users/avencastmini/PycharmProjects/EveNet/workspace/test_data/test_output/normalization.pt")
+particle_balance_dict = normalization_dict['particle_balance']
 
 # Load the Parquet file locally
 df = pq.read_table(
@@ -307,7 +308,7 @@ for iepoch in range(n_epoch):
                     targets_mask = batch["assignments-mask"],
                     num_targets = num_targets,
                     event_particles = event_particles,
-                    event_permutations = event_permutation,
+                    event_permutations = event_info.event_permutations,
                     focal_gamma =  0.1,
             )
 