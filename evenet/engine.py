fedd45dee02b7c7bf651c67b138085e53ebb6bc2
tihsu <b07202037@ntu.edu.tw>
2025-04-14 23:54:56 +0200

    [Generation] Update options

--- evenet/engine.py
+++ evenet/engine.py
@@ -78,6 +78,13 @@ class EveNetEngine(L.LightningModule):
 
             self.ass_args = get_ass(global_config.event_info)
 
+        #######  Initialize Diffusion Settings ##########
+        self.diffusion_every_n_epochs = global_config.options.Trainging.diffusion_every_n_epochs
+        self.diffusion_every_n_steps  = global_config.optioins.Training.diffusion_every_n_steps
+
+        self.global_diffusion_steps = self.component_cfg.GlobalGeneration.diffusion_steps
+        self.point_cloud_diffusion_steps =  self.component_cfg.EventGeneration.diffusion_steps
+
         ###### Initialize Loss ######
         self.cls_loss = None
         if self.classification_cfg.include:
@@ -218,7 +225,11 @@ class EveNetEngine(L.LightningModule):
                 model=self.model,
                 loss_scale=self.generation_cfg.loss_scale,
                 device=device,
-                diffusion_on= (not (self.training) and ((self.current_epoch%20) == 19) and ((self.global_step%5) == 0)) # TODO: hard coded now
+                num_steps_global=self.global_diffusion_steps,
+                num_steps_point_cloud=self.point_cloud_diffusion_steps,
+                diffusion_on= (not (self.training)
+                               and ((self.current_epoch%self.diffusion_every_n_epochs) == (self.diffusion_every_n_epochs-1))
+                               and ((self.global_step%self.diffusion_every_n_steps) == 0))
             )
             loss_head_dict["generation"] = scaled_gen_loss
             loss = loss + scaled_gen_loss
