3bdd895f3e703be1a1479c103cae791476e6a932
avencastmini <yulei@cern.ch>
2025-04-17 20:37:51 +0200

    Update submit_interactive.sh to include commented alternative salloc commands

--- NERSC/submit_interactive.sh
+++ NERSC/submit_interactive.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-salloc --nodes 4 --qos interactive --time 04:00:00 --constraint gpu --gpus 16 --account m2616_g --image=registry.nersc.gov/m2616/avencast/evenet:1.0
+#salloc --nodes 1 --qos interactive --time 04:00:00 --constraint cpu --account m2616 --image=registry.nersc.gov/m2616/avencast/evenet:1.0
+
+#salloc --nodes 4 --qos interactive --time 04:00:00 --constraint gpu --gpus 16 --account m2616_g --image=registry.nersc.gov/m2616/avencast/evenet:1.0
