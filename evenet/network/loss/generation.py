8a8c0bea6d861112b1b3b465cad8695191cb0988
avencastmini <yulei@cern.ch>
2025-04-29 14:55:42 +0200

    Refactor loss function definition for improved readability

--- evenet/network/loss/generation.py
+++ evenet/network/loss/generation.py
@@ -1,9 +1,13 @@
 import torch
 from torch import Tensor
 from typing import Optional
-def loss(predict: Tensor,
-         target: Tensor,
-         mask: Optional[Tensor] = None):
+
+
+def loss(
+        predict: Tensor,
+        target: Tensor,
+        mask: Optional[Tensor] = None
+):
     if mask is not None:
         feature_dim = predict.shape[-1]
         return torch.sum(((predict - target) ** 2) * mask.float()) / (torch.sum(mask.float()) * feature_dim)
