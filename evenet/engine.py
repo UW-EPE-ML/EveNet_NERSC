c7d070cbe26d5832b5dcceee70e1e5c9004e1ef5
avencastmini <yulei@cern.ch>
2025-05-10 14:57:25 +0200

    Refactor engine.py: modify logging of task losses to occur every 500 steps

--- evenet/engine.py
+++ evenet/engine.py
@@ -399,10 +399,12 @@ class EveNetEngine(L.LightningModule):
         #     check_every=1000,
         #     verbose=False,
         # )
-        for task, loss in task_losses.items():
-            print(f"[Task {task}] Loss: {loss.item()}")
-            # print_params_used_by_loss(loss, self.model)
-        self.log_task_gradient(task_losses, shared_params)
+        # for task, loss in task_losses.items():
+        #     print(f"[Task {task}] Loss: {loss.item()}")
+        #     print_params_used_by_loss(loss, self.model)
+
+        if self.current_step % 500 == 0:
+            self.log_task_gradient(task_losses, shared_params)
 
         # === Backward for EveNet Main Part (multitask) ===
         task_losses = list(task_losses.values())
