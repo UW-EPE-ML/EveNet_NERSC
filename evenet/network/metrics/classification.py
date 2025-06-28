5e608a6f43c8851e69739b0d1083ebfb428589e0
avencastmini <yulei@cern.ch>
2025-06-28 10:43:54 +0200

    Fix indexing in ROC logging and adjust training parameters in configuration

--- evenet/network/metrics/classification.py
+++ evenet/network/metrics/classification.py
@@ -355,24 +355,23 @@ def shared_epoch_end(
             })
             plt.close(fig_logits[i])
 
-            if i in fig_rocs:
+            if j in fig_rocs:
                 logger.log({
-                    f"{prefix}classification/roc_{class_name}": wandb.Image(fig_rocs[i])
+                    f"{prefix}classification/roc_{class_name}": wandb.Image(fig_rocs[j])
                 })
-                plt.close(fig_rocs[i])
-
-                logger.log({
-                    f"{prefix}classification/train_auc_{class_name}": aucs[i]
-                })
+                plt.close(fig_rocs[j])
 
                 logger.log({
-                    f"{prefix}classification/valid_auc_{class_name}": aucs_valid[i]
+                    f"{prefix}classification/train_auc_{class_name}": aucs[j]
                 })
 
-            if module is not None:
-                module.log(f"{prefix}classification/train_auc_{class_name}", aucs[i], prog_bar=True, sync_dist=False)
+                logger.log({
+                    f"{prefix}classification/valid_auc_{class_name}": aucs_valid[j]
+                })
 
-                module.log(f"{prefix}classification/valid_auc_{class_name}", aucs_valid[i], prog_bar=True, sync_dist=False)
+            # if module is not None:
+            #     module.log(f"{prefix}classification/train_auc_{class_name}", aucs[i], prog_bar=True, sync_dist=False)
+            #     module.log(f"{prefix}classification/valid_auc_{class_name}", aucs_valid[i], prog_bar=True, sync_dist=False)
 
     metrics_valid.reset(cm=True, logits=True)
     if metrics_train:
